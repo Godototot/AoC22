54e833cfd9b30170ec2766a8aa368e712ce03079
Godototot <red.godot@protonmail.com>
2022-12-12 14:00:57 +0100

    improved day 12 part 2

--- code/day12.py
+++ code/day12.py
@@ -13,7 +13,7 @@ def parse_char_as_int(char):
     elif char == 'E':
         return 25
     else:
-        return ord(char)-ord('a')
+        return ord(char) - ord('a')
 
 
 def prepare_input(input_file):
@@ -36,40 +36,35 @@ def prepare_input(input_file):
     return heightmap, start, end
 
 
-def find_shortest_distance(heightmap, start, end):
+def find_shortest_distance(heightmap, start, end, reverse=False):
     """ Calculates map with all min distances from given starting point
-    and returns the distance between start and end """
+        and returns the distance between start and end.
+        If reversed it gives distance between start and first node that has value 0 """
 
     pathmap = np.zeros(heightmap.shape, int)  # map to fill with min distance from start point
     pathmap[start] = 1  # start is set to 1 instead of zero, so the algorithm doesn't jump back to it
     next_nodes = deque()  # stack filled with all nodes that are adjacent to a calculated one, but are not calculated yet
     current_node = start
-
-    while not current_node == end:
-        # check if node is at the border
-        if current_node[0] > 0:
-            up_node = (current_node[0] - 1, current_node[1])
-            # check if the node still has to be calculated and if it is possible to move to it from the current node
-            if pathmap[up_node] == 0 and heightmap[up_node] <= heightmap[current_node] + 1:
-                # write in the distance from start to current node + 1
-                pathmap[up_node] = pathmap[current_node] + 1
-                next_nodes.appendleft(up_node)
-        if current_node[0] < (len(heightmap) - 1):
-            down_node = (current_node[0] + 1, current_node[1])
-            if pathmap[down_node] == 0 and heightmap[down_node] <= heightmap[current_node] + 1:
-                pathmap[down_node] = pathmap[current_node] + 1
-                next_nodes.appendleft(down_node)
-        if current_node[1] > 0:
-            left_node = (current_node[0], current_node[1] - 1)
-            if pathmap[left_node] == 0 and heightmap[left_node] <= heightmap[current_node] + 1:
-                pathmap[left_node] = pathmap[current_node] + 1
-                next_nodes.appendleft(left_node)
-        if current_node[1] < (len(heightmap[0]) - 1):
-            right_node = (current_node[0], current_node[1] + 1)
-            if pathmap[right_node] == 0 and heightmap[right_node] <= heightmap[current_node] + 1:
-                pathmap[right_node] = pathmap[current_node] + 1
-                next_nodes.appendleft(right_node)
-        # if there are no nodes left in 'next_nodes' but there is no other position then can be reached
+    # if reversed is false, it stops at the and, otherwise it stops if the current node is 'a'
+    while not ((not reverse and current_node == end) or
+               (reverse and heightmap[current_node] == 0)):
+        # calculate all neighbours
+        all_neighbours = [(current_node[0] - 1, current_node[1]),
+                          (current_node[0] + 1, current_node[1]),
+                          (current_node[0], current_node[1] + 1),
+                          (current_node[0], current_node[1] - 1)]
+        for neighbour in all_neighbours:
+            # checks if neighbour is out of scope
+            if 0 <= neighbour[0] < len(heightmap) and 0 <= neighbour[1] < len(heightmap[0]):
+                # checks if node was already calculated
+                if pathmap[neighbour] == 0:
+                    # check if the node can be reached by current node (or reversed)
+                    if (not reverse and heightmap[neighbour] <= heightmap[current_node] + 1) or \
+                            (reverse and heightmap[neighbour] + 1 >= heightmap[current_node]):
+                        # write in the distance from start to current node + 1
+                        pathmap[neighbour] = pathmap[current_node] + 1
+                        next_nodes.appendleft(neighbour)
+        # # if there are no nodes left in 'next_nodes' but there is no other position then can be reached
         # therefore the endpoint cannot be reached from the start and the distance can be represented as infinity
         if len(next_nodes) == 0:
             return math.inf
@@ -77,7 +72,7 @@ def find_shortest_distance(heightmap, start, end):
             # move to next node in the stack
             current_node = next_nodes.pop()
     # 1 has to be subtracted, since we started with distance 1 for the start position
-    return pathmap[end] - 1
+    return pathmap[current_node] - 1
 
 
 def part1(heightmap_data):
@@ -86,21 +81,16 @@ def part1(heightmap_data):
 
 
 def part2(heightmap_data):
+    # this part can be calculated in one go by starting at the end, reversing the pathfinder and stop if height is a
     heightmap, start, end = heightmap_data
-    all_paths = []
-    # iterates over all elements of the map and calculates the distance to the end point if the height is 0
-    for i in range(heightmap.shape[0]):
-        for j in range(heightmap.shape[1]):
-            if heightmap[i, j] == 0:
-                all_paths.append(find_shortest_distance(heightmap, (i, j), end))
-    return min(all_paths)
+    return find_shortest_distance(heightmap, end, start, True)
 
 
 def main() -> None:
     if len(sys.argv) > 2:
         input_file = sys.argv[2]
     else:
-        input_file = '../input/'+sys.argv[0][:-3]+'.txt'
+        input_file = '../input/' + sys.argv[0][:-3] + '.txt'
     if sys.argv[1] == '1':
         print(part1(prepare_input(input_file)))
     elif sys.argv[1] == '2':
