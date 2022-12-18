0632b0f74676334869611296248920cb864309d1
Godototot <red.godot@protonmail.com>
2022-12-18 17:28:49 +0100

    added day 18 day 2

--- code/day18.py
+++ code/day18.py
@@ -8,24 +8,72 @@ def prepare_input(input_file):
     return {(int(pixel[0]), int(pixel[1]), int(pixel[2])) for pixel in lines}
 
 
-sides = ((1, 0, 0), (-1, 0, 0),
+sides = {(1, 0, 0), (-1, 0, 0),
          (0, 1, 0), (0, -1, 0),
-         (0, 0, 1), (0, 0, -1))
+         (0, 0, 1), (0, 0, -1)}
 
 
-def add_sets(a, b):
-    return a[0]+b[0], a[1]+b[1], a[2]+b[2]
-
-
-def part1(pixel_list):
+def get_surface(pixel_list):
     surface = 0
     for pixel in pixel_list:
-        surface += sum(1 for s in sides if add_sets(pixel, s) not in pixel_list)
+        surface += sum(1 for s in sides if add_tuple(pixel, s) not in pixel_list)
     return surface
 
 
-def part2(input_data):
-    return ''
+def part1(pixel_list):
+    return get_surface(pixel_list)
+
+
+def get_negative(cube_size, pixel_list):
+    negative = set()
+    for x in range(cube_size[0]):
+        for y in range(cube_size[1]):
+            for z in range(cube_size[2]):
+                pixel = (x, y, z)
+                if pixel not in pixel_list:
+                    negative.add(pixel)
+    return negative
+
+
+def get_poisoned_walls(cube_size):
+    poisoned = set()
+    for y in range(cube_size[1]):
+        for z in range(cube_size[2]):
+            poisoned.add((-1, y, z))
+            poisoned.add((cube_size[0]+1, y, z))
+
+    for x in range(cube_size[1]):
+        for z in range(cube_size[2]):
+            poisoned.add((x, -1, z))
+            poisoned.add((x, cube_size[1]+1, z))
+
+    for x in range(cube_size[1]):
+        for y in range(cube_size[2]):
+            poisoned.add((x, y, -1))
+            poisoned.add((x, y, cube_size[2]+1))
+
+    return poisoned
+
+
+def part2(pixel_list):
+    max_x = max(pixel[0] for pixel in pixel_list)+1
+    max_y = max(pixel[1] for pixel in pixel_list)+1
+    max_z = max(pixel[2] for pixel in pixel_list)+1
+    air = get_negative((max_x, max_y, max_z), pixel_list)
+    poisoned = get_poisoned_walls((max_x, max_y, max_z))
+    while not len(poisoned) == 0:
+        new_poisoned = set()
+        new_air = set()
+        for pixel in air:
+            for s in sides:
+                if add_tuple(pixel, s) in poisoned:
+                    new_poisoned.add(pixel)
+                    break
+            else:
+                new_air.add(pixel)
+        poisoned = new_poisoned
+        air = new_air
+    return get_surface(pixel_list)-get_surface(air)
 
 
 def main() -> None:
--- code/helperfunc.py
+++ code/helperfunc.py
@@ -5,3 +5,11 @@ def read_input_lines(file_name):
     """takes file and returns strings for each line, removing the 'new line'"""
     lines = open(file_name, 'r').readlines()
     return list(map(methodcaller('replace', '\n', ''), lines))
+
+
+def add_tuple(a, b):
+    """ add each element of two tuples of same length"""
+
+    if len(a) != len(b):
+        raise Exception("Tuples are of different lengths")
+    return tuple(a[i]+b[i] for i in range(len(a)))
